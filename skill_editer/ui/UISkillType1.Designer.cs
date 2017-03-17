9ec620ed1a6c2803728fd5b9b505d5ff2c9b6d86
wuming <358591708@qq.com>
2017-03-18 00:11:34 +0800

    init

---
+++ skill_editer/ui/UISkillType1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace skill_editer.ui
+{
+    partial class UISkillType1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UISkillType1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UISkillType1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "UISkillType1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ skill_editer/ui/UISkillType1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace skill_editer.ui
+{
+    public partial class UISkillType1 : Form
+    {
+        public UISkillType1()
+        {
+            InitializeComponent();
+        }
+    }
+}
